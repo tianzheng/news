10115966d5ac1968be632a317a9d171ccec9b836
user <1426313137@qq.com>
2017-08-04 16:31:05 +0800

    线程

--- src/com/newcapec/news/Test.java
+++ src/com/newcapec/news/Test.java
@@ -6,52 +6,26 @@ package com.newcapec.news;
 public class Test {
 
 	public static void main(String[] args) {
-
-
-
-
-
 		System.out.println("杨克发");
-
-
-
-		
 		System.out.println("常凯华");
-
 		System.out.println("ccc");
-
 		System.out.println("Alex");
-
 		System.out.println("晋天正");
 		System.out.println("开始项目吧");
 		System.out.println("你好");
-		
 		System.out.println("(*^__^*) 嘻嘻……");
-
-		
-		System.out.println("starry的添加"
-				+ "添加"
-				+ "删除");
+		System.out.println("starry的添加"+ "添加"+ "删除");
 		System.out.println("=====");
 		System.out.println("周正123");
-
 		System.out.println("=====");
-
 		System.out.println("Newcapec");
 		System.out.println("新开普");
-		
-
 		System.out.println("Starry");
 		System.out.println("张星宇");
-		
 		System.out.println("Alex");
-
-
 		System.out.println("Alex");
 		System.out.println("suyuan");
-		
 		System.out.println("杨克发");
-		
 		System.out.println("Starry");
 		System.out.println("张星宇");
 		System.out.println("Alex");
