f5ab35bc8d8fd41527e75e99e32a0a11f83c240f
user <1426313137@qq.com>
2017-08-04 16:03:53 +0800

    添加

--- src/com/newcapec/news/Test.java
+++ src/com/newcapec/news/Test.java
@@ -3,10 +3,9 @@ package com.newcapec.news;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("项目");
-		System.out.println("开始项目！！");
 		
-		System.out.println("(*^__^*) 嘻嘻……");
+		System.out.println("常凯华");
+		
 	}
 
 }
