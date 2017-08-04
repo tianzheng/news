20b8823dae37a91dfc8d5deaacd01f50d95a91b5
Administrator <1929906061@qq.com>
2017-08-04 16:06:09 +0800

    名字

--- src/com/newcapec/news/Test.java
+++ src/com/newcapec/news/Test.java
@@ -3,15 +3,7 @@ package com.newcapec.news;
 public class Test {
 
 	public static void main(String[] args) {
-
-		// TODO Auto-generated method stub
-
-		System.out.println("开始吧");
-
-		System.out.println("开始项目吧");
-		System.out.println("(*^__^*) 嘻嘻……");
 		System.out.println("高增辉");
-
 	}
 
 }
