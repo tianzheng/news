0100d8b68f03091731daa06c5624fea843a5de62
tianzheng <jintian_2004@163.com>
2017-08-04 16:01:29 +0800

    添加信息

--- src/com/newcapec/news/Test.java
+++ src/com/newcapec/news/Test.java
@@ -6,14 +6,19 @@ public class Test {
 		System.out.println("晋天正");
 		System.out.println("开始项目吧");
 		System.out.println("(*^__^*) 嘻嘻……");
-<<<<<<< HEAD
+
 		
 		System.out.println("starry的添加"
 				+ "添加"
 				+ "删除");
-=======
+
 		System.out.println("=====");
->>>>>>> 1f73d7cd6130adb84835c7f8be301e00a13a9c59
+
+		System.out.println("Newcapec");
+		System.out.println("新开普");
+		
+		
+		
 	}
 	
 
