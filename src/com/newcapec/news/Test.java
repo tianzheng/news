23be14e5e1beeee9c1c57a57e7cd1fd3ad7a1a78
KFYoung <744551214@qq.com>
2017-08-04 15:55:42 +0800

    添加

--- src/com/newcapec/news/Test.java
+++ src/com/newcapec/news/Test.java
@@ -1,6 +1,6 @@
 package com.newcapec.news;
 
-import java.util.Date;
+
 
 public class Test {
 
@@ -9,7 +9,7 @@ public class Test {
 		System.out.println("let's begin");
 		System.out.println("开始项目吧");
 		System.out.println("(*^__^*) 嘻嘻……");
-		System.out.println(new Date());
+		System.out.println("==========");
 	}
 
 }
