50dfcc5e5025c6367cb9c338990536d59e62462a
news <7953810@qq.com>
2017-08-04 15:55:59 +0800

    添加删除

--- src/com/newcapec/news/Test.java
+++ src/com/newcapec/news/Test.java
@@ -6,7 +6,9 @@ public class Test {
 		System.out.println("开始项目吧");
 		System.out.println("(*^__^*) 嘻嘻……");
 		
-		System.out.println("starry的添加");
+		System.out.println("starry的添加"
+				+ "添加"
+				+ "删除");
 	}
 
 }
