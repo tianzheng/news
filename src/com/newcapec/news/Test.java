9765aa3d23ac89bd04d851f9fc5a63ca3d562a17
df41cn <443731448@qq.com>
2017-08-04 15:55:43 +0800

    C盘提交

--- src/com/newcapec/news/Test.java
+++ src/com/newcapec/news/Test.java
@@ -6,6 +6,7 @@ public class Test {
 		System.out.println("开始项目吧");
 		System.out.println("(*^__^*) 嘻嘻……");
 		System.out.println("瞬间爆炸....");
+		System.out.println("C:\\Users\\Administrator\\git\\news");
 	}
 
 }
