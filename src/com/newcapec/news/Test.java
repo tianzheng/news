5dd96f25047d6c85a724bf9ac1aef49f6c83952a
df41cn <443731448@qq.com>
2017-08-04 16:16:52 +0800

    提交

--- src/com/newcapec/news/Test.java
+++ src/com/newcapec/news/Test.java
@@ -3,24 +3,11 @@ package com.newcapec.news;
 public class Test {
 
 	public static void main(String[] args) {
-
-		
 		System.out.println("常凯华");
-
 		System.out.println("Alex");
-
 		System.out.println("晋天正");
 		System.out.println("开始项目吧");
 		System.out.println("你好");
-		
-		System.out.println("(*^__^*) 嘻嘻……");
-		System.out.println("瞬间爆炸....");
-		System.out.println("C:\\Users\\Administrator\\git\\news");
-		System.out.println("=====");
-		System.out.println("王东方");
-		System.out.println("starry的添加"
-				+ "添加"
-				+ "删除");
 		System.out.println("=====");
 		System.out.println("周正123");
 		System.out.println("=====");
@@ -31,8 +18,6 @@ public class Test {
 		System.out.println("张星宇");
 		System.out.println("Alex");
 		System.out.println("王东方");
-		
-		
 	}
 	
 
